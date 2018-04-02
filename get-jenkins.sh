b00d0cfdf0a3a34278468e198d413515d071d1cd
Aivo Antsman <antsman@users.noreply.github.com>
2018-04-02 19:16:28 +0300

    Adding get-jenkins.sh

--- get-jenkins.sh
+++ get-jenkins.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# get qemu-arm-static
-wget -nc https://github.com/multiarch/qemu-user-static/releases/download/v2.11.0/qemu-arm-static && chmod +x qemu-arm-static
-
-# get jengins
+# get Jenkins
 wget -nc http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war
 
-# get jenkins version
+# get Jenkins version
 export JENKINS_VERSION=$(unzip -p jenkins.war META-INF/MANIFEST.MF | grep Jenkins-Version | awk '{print $2}')
 echo Jenkins-Version: $JENKINS_VERSION
 
-# docker build -t rpi-jenkins .
+# get qemu-arm-static - using with Travis
+wget -nc https://github.com/multiarch/qemu-user-static/releases/download/v2.11.0/qemu-arm-static && chmod +x qemu-arm-static
