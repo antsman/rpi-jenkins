405588b4a3b8d19c99ad1cf3537ad4f43d7d09ed
Aivo Antsman <antsman@users.noreply.github.com>
2018-04-03 15:53:12 +0300

    Update Jenkinsfile

--- get-jenkins.sh
+++ get-jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # get Jenkins
-wget -nc http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war
+wget -nc -nv http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war
 
 # get Jenkins version
 export JENKINS_VERSION=$(unzip -p jenkins.war META-INF/MANIFEST.MF | grep Jenkins-Version | awk '{print $2}')
 echo Jenkins-Version: $JENKINS_VERSION
 
 # get qemu-arm-static - using with Travis
-wget -nc https://github.com/multiarch/qemu-user-static/releases/download/v2.11.0/qemu-arm-static && chmod +x qemu-arm-static
+wget -nc -nv https://github.com/multiarch/qemu-user-static/releases/download/v2.11.0/qemu-arm-static && chmod +x qemu-arm-static
