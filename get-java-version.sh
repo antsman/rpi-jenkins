c88f44daaeb3180a499611a4713596cb27c0ad2d
Aivo Antsman <antsman@users.noreply.github.com>
2018-04-17 21:32:55 +0300

    Update Jenkinsfile

--- get-java-version.sh
+++ get-java-version.sh
@@ -4,5 +4,6 @@ test -n "$1" && (
     echo JAVA_VERSION=\"$(docker exec -t $1 java -version | grep version | awk -F\" '{ print $2 }')\" | tee -a env.properties
 ) || (
     echo Give container name as parameter!
+    exit 1
 )
 
