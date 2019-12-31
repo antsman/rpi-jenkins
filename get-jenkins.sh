e4789cfea455af77b10276d580bb8e0df2663421
Aivo Antsman <aivo.antsman@gmail.com>
2019-12-31 21:43:27 +0200

    Update get-jenkins.sh

--- get-jenkins.sh
+++ get-jenkins.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # get jenkins
-wget -nc http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 
 # find Jenkins version
 echo JENKINS_VERSION=\"$(unzip -p jenkins.war META-INF/MANIFEST.MF | grep Jenkins-Version | awk '{printf $2}'| tr -d '\r')\" | tee env.properties
 
 # get docker binaries
 DOCKER_VERSION=19.03.5
-wget -nc https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
+wget --timestamping --no-verbose https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
 tar xzvf docker-$DOCKER_VERSION.tgz --strip 1 docker/docker
 
 # get qemu-arm-static - using with Travis
