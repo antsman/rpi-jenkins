a79a3a10fb4ee246b62c5055ecfb4a42519a6bfb
Aivo Antsman <aivo.antsman@gmail.com>
2020-01-01 16:42:10 +0200

    Update get-java-version.sh, add jenkins & docker

--- get-java-version.sh
+++ get-java-version.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+VERSIONS=version.properties
+
 test -n "$1" && (
-    echo JAVA_VERSION=\"$(docker exec -t $1 java -version | grep version | awk -F\" '{ print $2 }')\" | tee -a env.properties
+    echo JENKINS_VERSION=\"$(docker exec -t $1 cat /data/war/META-INF/MANIFEST.MF | grep Jenkins-Version | awk '{ print $2 }' | tr -d '\r')\" | tee $VERSIONS
+    echo JAVA_VERSION=\"$(docker exec -t $1 java -version | grep version | awk -F\" '{ print $2 }')\" | tee -a $VERSIONS
+    echo DOCKER_VERSION=\"$(docker exec -t $1 docker --version | grep version | awk '{ print $3 }' | tr -d ',')\" | tee -a $VERSIONS
 ) || (
     echo Give container name as parameter!
     exit 1
 )
+
