61713020afbe631c2ab7f2cfcc3b3270f04c580b
Aivo Antsman <aivo.antsman@gmail.com>
2020-03-10 00:46:42 +0200

    Update Docker to 19.03.7

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -5,7 +5,7 @@ JENKINS_VERSION=2.204.5
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
-DOCKER_VERSION=19.03.5
+DOCKER_VERSION=19.03.7
 wget --timestamping --no-verbose https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
 tar xzvf docker-$DOCKER_VERSION.tgz --strip 1 docker/docker
 
