5ce8ec1d20532056a7abec6f18f2b07386c2b116
Aivo Antsman <aivo.antsman@gmail.com>
2020-08-20 01:35:52 +0300

    All echo-s in one tee

--- get-versions.sh
+++ get-versions.sh
@@ -5,10 +5,10 @@
 VERSIONS=version.properties
 
 test -n "$1" && (
-    echo JENKINS_VERSION=\"$(docker exec -t $1 cat /data/war/META-INF/MANIFEST.MF | grep Jenkins-Version | awk '{ print $2 }' | tr -d '\r')\" | tee $VERSIONS
-    echo JAVA_VERSION=\"$(docker exec -t $1 java -version | grep version | awk -F\" '{ print $2 }')\" | tee -a $VERSIONS
-    echo DOCKER_VERSION=\"$(docker exec -t $1 docker --version | grep version | awk '{ print $3 }' | tr -d ',')\" | tee -a $VERSIONS
-) || (
+    echo JENKINS_VERSION=\"$(docker exec -t $1 cat /data/war/META-INF/MANIFEST.MF | grep Jenkins-Version | awk '{ print $2 }' | tr -d '\r')\"
+    echo JAVA_VERSION=\"$(docker exec -t $1 java -version | grep version | awk -F\" '{ print $2 }')\"
+    echo DOCKER_VERSION=\"$(docker exec -t $1 docker --version | grep version | awk '{ print $3 }' | tr -d ',')\"
+) | tee $VERSIONS || (
     echo Give container name as parameter!
     exit 1
 )
