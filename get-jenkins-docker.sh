cbda6a498d6ec30b850d4e761b91039d5aa4cd51
Aivo Antsman <aivo.antsman@gmail.com>
2020-09-12 12:07:38 +0300

    Update Docker client to 19.03.12

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -5,6 +5,6 @@ JENKINS_VERSION=2.249.1
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
-DOCKER_VERSION=19.03.9
+DOCKER_VERSION=19.03.12
 wget --timestamping --no-verbose https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
 tar xzvf docker-$DOCKER_VERSION.tgz --strip 1 docker/docker
