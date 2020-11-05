3ebc7dfb0e8c8423f7f9fb1915ae1159797199e4
Aivo Antsman <aivo.antsman@gmail.com>
2020-11-05 22:56:47 +0200

    Update Jenkins to 2.249.3

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get jenkins
-JENKINS_VERSION=2.249.2
+JENKINS_VERSION=2.249.3
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
