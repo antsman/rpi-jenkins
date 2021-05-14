11e1cbfa4c8e905211ae5fd7d42bb4595c78d5f4
Aivo Antsman <aivo.antsman@gmail.com>
2021-05-14 21:11:57 +0300

    Update Jenkins to 2.277.4

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get jenkins
-JENKINS_VERSION=2.277.3
+JENKINS_VERSION=2.277.4
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
