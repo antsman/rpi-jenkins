02961f32d39981f6fca340f6c59b543dc009f783
Aivo Antsman <aivo.antsman@gmail.com>
2021-06-13 13:03:36 +0300

    Update Docker client to 20.10.7

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -5,6 +5,6 @@ JENKINS_VERSION=2.289.1
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
-DOCKER_VERSION=20.10.6
+DOCKER_VERSION=20.10.7
 wget --timestamping --no-verbose https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
 tar xzvf docker-$DOCKER_VERSION.tgz --strip 1 docker/docker
