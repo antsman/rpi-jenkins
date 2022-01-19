1b69bc31f93bbd6374caa58968f598a054fb6b76
Aivo Antsman <aivo.antsman@gmail.com>
2022-01-19 20:15:05 +0200

    Update Docker client to 20.10.12

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -5,6 +5,6 @@ JENKINS_VERSION=2.319.1
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
-DOCKER_VERSION=20.10.11
+DOCKER_VERSION=20.10.12
 wget --timestamping --no-verbose https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
 tar xzvf docker-$DOCKER_VERSION.tgz --strip 1 docker/docker
