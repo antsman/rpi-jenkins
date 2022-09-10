a0e250412363b1172cb454d05a5fab97b98188ee
Aivo Antsman <aivo.antsman@gmail.com>
2022-09-10 17:10:27 +0300

    Update Jenkins to 2.361.1

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get jenkins
-JENKINS_VERSION=2.346.3
+JENKINS_VERSION=2.361.1
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
