b755be7d000c2cff375365e7bbd8b1a747d58286
Aivo Antsman <aivo.antsman@gmail.com>
2023-03-10 21:22:00 +0200

    Update Docker client to 23.0.1

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -5,6 +5,6 @@ JENKINS_VERSION=2.387.1
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
-DOCKER_VERSION=20.10.23
+DOCKER_VERSION=23.0.1
 wget --timestamping --no-verbose https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
 tar xzvf docker-$DOCKER_VERSION.tgz --strip 1 docker/docker
