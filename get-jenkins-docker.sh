c8612d875235399ebee0152ce79d725a5aae4f9f
Aivo Antsman <aivo.antsman@gmail.com>
2023-08-03 23:35:21 +0300

    Update Docker client to 24.0.5

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -5,6 +5,6 @@ JENKINS_VERSION=2.401.3
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
-DOCKER_VERSION=23.0.1
+DOCKER_VERSION=24.0.5
 wget --timestamping --no-verbose https://download.docker.com/linux/static/stable/armhf/docker-$DOCKER_VERSION.tgz
 tar xzvf docker-$DOCKER_VERSION.tgz --strip 1 docker/docker
