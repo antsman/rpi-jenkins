77bffc18ee21218246e1c36d233b4493ba953624
Aivo Antsman <aivo.antsman@gmail.com>
2024-06-29 13:11:17 +0300

    Update Docker client to 27.0.2

--- get-jenkins-docker.sh
+++ get-jenkins-docker.sh
@@ -5,6 +5,6 @@ JENKINS_VERSION=2.426.2
 wget --timestamping --no-verbose http://mirrors.jenkins.io/war-stable/$JENKINS_VERSION/jenkins.war
 
 # get docker binaries
-DOCKER_VERSION=24.0.7
+DOCKER_VERSION=27.0.2
 wget --timestamping --no-verbose https://download.docker.com/linux/static/stable/aarch64/docker-$DOCKER_VERSION.tgz
 tar xzvf docker-$DOCKER_VERSION.tgz --strip 1 docker/docker
